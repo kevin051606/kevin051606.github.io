d8e9ed709d4af127796452d792890daab2f1e52b
K065 <77740459+kevin051606@users.noreply.github.com>
2021-09-30 13:28:45 -0400

    Create indexJs.js

---
+++ indexJs.js
@@ -0,0 +1,6 @@
+function unhide(){
+  let normalDiv = document.getElementById("normal");
+  let fakeDiv = document.getElementById("haha");
+  fakeDiv.visibility = hidden;
+  normalDiv.visibility = visible;
+}
