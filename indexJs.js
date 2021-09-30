a3eb735093226e3e5623196ffdf16eb7471e0ff0
K065 <77740459+kevin051606@users.noreply.github.com>
2021-09-30 13:43:39 -0400

    Update indexJs.js

--- indexJs.js
+++ indexJs.js
@@ -1,6 +1,6 @@
 function unhide(){
   let normalDiv = document.getElementById("normal");
   let fakeDiv = document.getElementById("haha");
-  fakeDiv.visibility = hidden;
-  normalDiv.visibility = visible;
+  fakeDiv.visibility = "hidden";
+  normalDiv.visibility = "visible";
 }
